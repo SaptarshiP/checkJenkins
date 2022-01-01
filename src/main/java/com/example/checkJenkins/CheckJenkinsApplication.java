1ce75666161223f5c054971ea16e54491fd5d7cc
DESKTOP-J05IIIT\SAPTARSHI <|Saptarshi1295@gmail.com>
2022-01-01 19:57:11 +0530

    Firt commit

---
+++ src/main/java/com/example/checkJenkins/CheckJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.checkJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/checkJenkins/CheckJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
